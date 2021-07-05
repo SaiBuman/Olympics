81806d0fcfeae567a5a2ab8b6fe81199bee1a2eb
Sai Buman <78092854+SaiBuman@users.noreply.github.com>
2021-07-05 22:09:38 +0530

    Update C81main.js

--- C81main.js
+++ C81main.js
@@ -1,20 +1,16 @@
 canvas = document.getElementById("my_canvas");
-
 ctx1 = canvas.getContext("2d");
 ctx1.beginPath();
 ctx1.strokeStyle = blue;
 ctx1.lineWidth = 2;
 ctx1.arc(200,200,40,0,2 * Math.PI);
 ctx1.stroke();
-canvas.addEventListener("mousedown", my_mousedown);
 
-ctx1 = canvas.getContext("2d");
 ctx1.beginPath();
 ctx1.strokeStyle = yellow;
 ctx1.lineWidth = 2;
-ctx1.arc(250,300,40,0,2 * Math.PI);
+ctx1.arc(250,225,40,0,2 * Math.PI);
 ctx1.stroke();
-canvas.addEventListener("mousedown", my_mousedown);
 
 ctx1 = canvas.getContext("2d");
 ctx1.beginPath();
@@ -22,15 +18,13 @@ ctx1.strokeStyle = black;
 ctx1.lineWidth = 2;
 ctx1.arc(300,200,40,0,2 * Math.PI);
 ctx1.stroke();
-canvas.addEventListener("mousedown", my_mousedown);
 
 ctx1 = canvas.getContext("2d");
 ctx1.beginPath();
 ctx1.strokeStyle = green;
 ctx1.lineWidth = 2;
-ctx1.arc(350,300,40,0,2 * Math.PI);
+ctx1.arc(350,225,40,0,2 * Math.PI);
 ctx1.stroke();
-canvas.addEventListener("mousedown", my_mousedown);
 
 
 ctx1 = canvas.getContext("2d");
@@ -41,10 +35,3 @@ ctx1.arc(400,200,40,0,2 * Math.PI);
 ctx1.stroke();
 
 
-function circle1() {
-    ctx.beginPath();
-    ctx.strokeStyle = ctx1.strokeStyle ;
-    ctx.lineWidth = 2;
-    ctx.arc(200, 200, 40, 0, 2 * Math.PI);
-    ctx.stroke();
-}
